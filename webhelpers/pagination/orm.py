e7c69b90f0051197bc04953a802680111a96f80c
bbangert <bbangert@localhost>
2006-03-04 21:00:13 -0800

    [svn] Tests SQLObject classness, also can handle being passed either an SQLAlchemy Table class, or an SQLAlchemy Mapper object.

--- webhelpers/pagination/orm.py
+++ webhelpers/pagination/orm.py
@@ -1,4 +1,5 @@
 """ORM Wrappers"""
+import inspect
 from webhelpers.util import Partial
 
 orms = {}
@@ -19,10 +20,15 @@ def get_wrapper(obj, *args, **kw):
     if isinstance(obj, (list, tuple)):
         return obj
     if orms.get('sqlobject'):
-        if issubclass(obj, sqlobject.SQLObject):
+        if inspect.isclass(obj) and issubclass(obj, sqlobject.SQLObject):
             return SQLObjectLazy(obj.select, *args, **kw)
     if orms.get('sqlalchemy'):
-        return SQLAlchemyLazy(obj.select, *args, **kw)
+        if hasattr(obj, '_is_primary_mapper'):
+            return SQLAlchemyLazyMapper(obj, *args, **kw)
+        if isinstance(obj, sqlalchemy.Table):
+            return SQLAlchemyLazyTable(obj, *args, **kw)
+    return "You shouldn't have this"
+    
 
 class SQLObjectLazy(Partial):
     def __getitem__(self, key):
@@ -33,14 +39,33 @@ class SQLObjectLazy(Partial):
     def __len__(self):
         return self().count()
 
-class SQLAlchemyLazy(Partial):
+class SQLAlchemyLazyTable(Partial):
     def __getitem__(self, key):
         if not isinstance(key, slice):
             raise Exception, "SQLAlchemyLazy doesn't support getitem without slicing"
-        limit = key.stop - key.start + 1
+        limit = key.stop - key.start
         offset = key.start
-        return self(limit=limit, offset=offset).execute()
+        fn = self.fn
+        self.fn = fn.select
+        results = self(limit=limit, offset=offset).execute()
+        self.fn = fn
+        return results
     
     def __len__(self):
-        s = self()
-        return self.fn([func.count(1)], from_obj=[s]).execute()
+        s = self.fn.select(*self.args, **self.kw)
+        return self.fn([func.count(1)], from_obj=[s])
+
+class SQLAlchemyLazyMapper(Partial):
+    def __getitem__(self, key):
+        if not isinstance(key, slice):
+            raise Exception, "SQLAlchemyLazy doesn't support getitem without slicing"
+        limit = key.stop - key.start
+        offset = key.start
+        fn = self.fn
+        self.fn = fn.select
+        results = self(limit=limit, offset=offset)
+        self.fn = fn
+        return results
+    
+    def __len__(self):
+        return self.fn.count(*self.args, **self.kw)
