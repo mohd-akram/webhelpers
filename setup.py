fe779b23b92a791eb3a0f4d10dc72b96f7ca6c82
bbangert <bbangert@localhost>
2006-10-20 10:15:51 -0700

    [svn] Bumping trunk to 0.2.3 dev.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="WebHelpers",
-    version='0.2.2',
+    version='0.2.3',
     description='Web Helpers',
     long_description="""
 Web Helpers is a library of helper functions intended to make writing templates in web
