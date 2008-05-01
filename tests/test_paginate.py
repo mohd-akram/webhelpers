ee584ccec1c48883ec87d79ac11424d628db040b
Christoph Haas <email@christoph-haas.de>
2008-05-01 17:42:58 +0200

    Parameters renamed. Item indices start at 1 instead of 0.

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -8,8 +8,8 @@ from webhelpers.paginate import Page
 def test_empty_list():
     """Test whether an empty list is handled correctly."""
     items = []
-    page = Page(items, current_page=0)
-    assert page.current_page == 0
+    page = Page(items, page=0)
+    assert page.page == 0
     assert page.first_item is None
     assert page.last_item is None
     assert page.first_page is None
@@ -25,10 +25,10 @@ def test_empty_list():
 def test_one_page():
     """Test that we fit 10 items on a single 10-item page."""
     items = range(10)
-    page = Page(items, current_page=0, items_per_page=10)
-    assert page.current_page == 1
-    assert page.first_item == 0
-    assert page.last_item == 9
+    page = Page(items, page=0, items_per_page=10)
+    assert page.page == 1
+    assert page.first_item == 1
+    assert page.last_item == 10
     assert page.first_page == 1
     assert page.last_page == 1
     assert page.previous_page is None
@@ -47,10 +47,10 @@ def test_many_pages():
     mapper.connect(':controller')
 
     items = range(100)
-    page = Page(items, current_page=0, items_per_page=15)
-    assert page.current_page == 1
-    assert page.first_item == 0
-    assert page.last_item == 14
+    page = Page(items, page=0, items_per_page=15)
+    assert page.page == 1
+    assert page.first_item == 1
+    assert page.last_item == 15
     assert page.first_page == 1
     assert page.last_page == 7
     assert page.previous_page is None
@@ -58,8 +58,9 @@ def test_many_pages():
     assert page.items_per_page == 15
     assert page.item_count == 100
     assert page.page_count == 7
-    assert page.pager() == '<span class="pager_curpage">1</span> <a class="pager_link" href="/content?page_nr=2">2</a> <a class="pager_link" href="/content?page_nr=3">3</a> <span class="pager_dotdot">..</span> <a class="pager_link" href="/content?page_nr=7">7</a>'
-    assert page.pager(separator='_') == '<span class="pager_curpage">1</span>_<a class="pager_link" href="/content?page_nr=2">2</a>_<a class="pager_link" href="/content?page_nr=3">3</a>_<span class="pager_dotdot">..</span>_<a class="pager_link" href="/content?page_nr=7">7</a>'
-    assert page.pager(link_var='xy') == '<span class="pager_curpage">1</span> <a class="pager_link" href="/content?xy=2">2</a> <a class="pager_link" href="/content?xy=3">3</a> <span class="pager_dotdot">..</span> <a class="pager_link" href="/content?xy=7">7</a>'
-    assert page.pager(link_attr={'style':'s1'}, curpage_attr={'style':'s2'}, dotdot_attr={'style':'s3'}) == '<span style="s2">1</span> <a href="/content?page_nr=2" style="s1">2</a> <a href="/content?page_nr=3" style="s1">3</a> <span style="s3">..</span> <a href="/content?page_nr=7" style="s1">7</a>'
+    print page.pager()
+    assert page.pager() == '<span class="pager_curpage">1</span> <a class="pager_link" href="/content?page=2">2</a> <a class="pager_link" href="/content?page=3">3</a> <span class="pager_dotdot">..</span> <a class="pager_link" href="/content?page=7">7</a>'
+    assert page.pager(separator='_') == '<span class="pager_curpage">1</span>_<a class="pager_link" href="/content?page=2">2</a>_<a class="pager_link" href="/content?page=3">3</a>_<span class="pager_dotdot">..</span>_<a class="pager_link" href="/content?page=7">7</a>'
+    assert page.pager(page_param='xy') == '<span class="pager_curpage">1</span> <a class="pager_link" href="/content?xy=2">2</a> <a class="pager_link" href="/content?xy=3">3</a> <span class="pager_dotdot">..</span> <a class="pager_link" href="/content?xy=7">7</a>'
+    assert page.pager(link_attr={'style':'s1'}, curpage_attr={'style':'s2'}, dotdot_attr={'style':'s3'}) == '<span style="s2">1</span> <a href="/content?page=2" style="s1">2</a> <a href="/content?page=3" style="s1">3</a> <span style="s3">..</span> <a href="/content?page=7" style="s1">7</a>'
 
