6e052b397726dabcb7bc70460ab45a988f8ed19b
Ben Bangert <ben@groovie.org>
2008-06-13 10:35:15 -0700

    Switch to using eq_

--- tests/test_mimetypes.py
+++ tests/test_mimetypes.py
@@ -1,4 +1,5 @@
 from nose.plugins.skip import SkipTest
+from nose.tools import eq_
 
 from webhelpers.mimehelper import MIMETypes
 from util import test_environ
@@ -14,14 +15,14 @@ def setup():
 
 def test_register_alias():
     MIMETypes.add_alias('html', 'text/html')
-    assert MIMETypes.aliases['html'] == 'text/html'
+    eq_(MIMETypes.aliases['html'], 'text/html')
     
 def test_usage():
     _check_webob_dependency()
     environ = test_environ()
     environ['PATH_INFO'] = '/test.html'
     m = MIMETypes(environ)
-    assert m.mimetype('html') == 'text/html'
+    eq_(m.mimetype('html'), 'text/html')
 
 def test_root_path():
     _check_webob_dependency()
@@ -29,16 +30,16 @@ def test_root_path():
     environ['PATH_INFO'] = '/'
     environ['HTTP_ACCEPT'] = 'text/html, application/xml'
     m = MIMETypes(environ)
-    assert m.mimetype('text/html') == 'text/html'
+    eq_(m.mimetype('text/html'), 'text/html')
 
-def test_with_extention():
+def test_with_extension():
     _check_webob_dependency()
     environ = test_environ()
     environ['PATH_INFO'] = '/test.xml'
     environ['HTTP_ACCEPT'] = 'text/html, application/xml'
     m = MIMETypes(environ)
-    assert m.mimetype('text/html') == False
-    assert m.mimetype('application/xml') == 'application/xml'
+    eq_(m.mimetype('text/html'), False)
+    eq_(m.mimetype('application/xml'), 'application/xml')
 
 def test_with_unregistered_extention():
     _check_webob_dependency()
@@ -46,8 +47,8 @@ def test_with_unregistered_extention():
     environ['PATH_INFO'] = '/test.iscool'
     environ['HTTP_ACCEPT'] = 'application/xml'
     m = MIMETypes(environ)
-    assert m.mimetype('text/html') == False
-    assert m.mimetype('application/xml') == 'application/xml'
+    eq_(m.mimetype('text/html'), False)
+    eq_(m.mimetype('application/xml'), 'application/xml')
 
 def test_with_no_extention():
     _check_webob_dependency()
@@ -55,15 +56,15 @@ def test_with_no_extention():
     environ['PATH_INFO'] = '/test'
     environ['HTTP_ACCEPT'] = 'application/xml'
     m = MIMETypes(environ)
-    assert m.mimetype('text/html') == False
-    assert m.mimetype('application/xml') == 'application/xml'
+    eq_(m.mimetype('text/html'), False)
+    eq_(m.mimetype('application/xml'), 'application/xml')
     
 def test_with_no_extention_and_no_accept():
     _check_webob_dependency()
     environ = test_environ()
     environ['PATH_INFO'] = '/test'
     m = MIMETypes(environ)
-    assert m.mimetype('html') == 'text/html'
+    eq_(m.mimetype('html'), 'text/html')
 
 def test_with_text_star_accept():
     _check_webob_dependency()
@@ -71,7 +72,7 @@ def test_with_text_star_accept():
     environ['PATH_INFO'] = '/test.iscool'
     environ['HTTP_ACCEPT'] = 'text/*'
     m = MIMETypes(environ)
-    assert m.mimetype('text/html') == 'text/html'
+    eq_(m.mimetype('text/html'), 'text/html')
 
 def test_with_star_star_accept():
     _check_webob_dependency()
@@ -79,4 +80,4 @@ def test_with_star_star_accept():
     environ['PATH_INFO'] = '/test.iscool'
     environ['HTTP_ACCEPT'] = '*/*'
     m = MIMETypes(environ)
-    assert m.mimetype('application/xml') == 'application/xml'
+    eq_(m.mimetype('application/xml'), 'application/xml')
