fd7a1ee1be35201abb583fc258710a9c0d9197c1
Mike Orr <sluggoster@gmail.com>
2008-07-28 14:29:59 -0700

    Escape nl2hr() argument to prevent stray HTML from passing through.

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -26,6 +26,12 @@ Acknowledgements <a id="acknowledgements" />
 def test_nl2br():
     eq_(u'A B<br />\nC D<br />\n<br />\nE F', nl2br("A B\nC D\r\n\r\nE F"))
 
+def test_nl2br2():
+    eq_(u'&lt;strike&gt;W&lt;/strike&gt;<br />\nThe W', nl2br("<strike>W</strike>\nThe W"))
+
+def test_nl2br3():
+    eq_(u'<strike>W</strike><br />\nThe W', nl2br(literal("<strike>W</strike>\nThe W")))
+
 def test_format_paragraphs1():
     eq_(u"<p>crazy\n cross\n platform linebreaks</p>", format_paragraphs("crazy\r\n cross\r platform linebreaks"))
 
