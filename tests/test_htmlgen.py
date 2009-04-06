a0fef25d16dd4b665c4e2ccac0c53e82efc6469a
Philip Jenvey <pjenvey@underboss.org>
2009-04-06 13:58:22 -0700

    don't assume dict ordering

--- tests/test_htmlgen.py
+++ tests/test_htmlgen.py
@@ -6,5 +6,5 @@ def test_html():
     assert str(html.br) == '<br />'
     assert html.a(href='url', c='whatever') == '<a href="url">whatever</a>'
     assert html.a('whatever', href='url', class_=None) == '<a href="url">whatever</a>'
-    assert html.a(a=1, b=2, c_=3) == '<a a="1" b="2" c="3"></a>'
+    assert html.a(a=1, b_=2) in ('<a a="1" b="2"></a>', '<a b="2" a="1"></a>')
     
