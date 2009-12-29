90ede7fe64d869da406b0a92632c78ab53e8915e
ergo <ergo@localhost>
2009-12-29 22:14:47 +0100

    pep8 and request is now required arg for GridPylons

--- webhelpers/pylonslib/grid.py
+++ webhelpers/pylonslib/grid.py
@@ -6,12 +6,13 @@ class NoRequestError(Exception):
 
 class GridPylons(grid.Grid):
     
+    def __init__(self, request, *args, **kw):
+        self.request = request
+        super(GridPylons, self).__init__(*args, **kw)
+    
     def generate_header_link(self, column_number, column, label_text):
         from pylons import url
         # this will handle possible URL generation
-        if not hasattr(self, "request"):
-            raise NoRequestError(
-                "Could not find self.request for this grid")
         request_copy = self.request.copy().GET
         if "order_col" in request_copy and "order_dir" in request_copy:
             self.order_column = request_copy.pop("order_col")
@@ -25,11 +26,14 @@ class GridPylons(grid.Grid):
         else:
             new_order_dir = "asc"
         
-        url_href = url.current(order_col=column, order_dir=new_order_dir, **request_copy)        
+        url_href = url.current(order_col=column, order_dir=new_order_dir,
+                               **request_copy)
         label_text = HTML.tag("a", href=url_href, c=label_text)
         # Is the current column the one we're ordering on?
         if (column == self.order_column):
-            return self.default_header_ordered_column_format(column_number, self.order_dir,
+            return self.default_header_ordered_column_format(column_number,
+                                                             self.order_dir,
                                                              column, label_text)
         else:
-            return self.default_header_column_format(column_number, column, label_text)
+            return self.default_header_column_format(column_number, column,
+                                                     label_text)
