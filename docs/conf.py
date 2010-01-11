f1046256c56950d173e3a6d5e9649c37ebe10e9f
sluggoster <sluggoster@gmail.com>
2010-01-11 02:29:14 -0800

    Home page docs.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ copyright = '2009, Ben Bangert and Mike Orr'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0b1'
+version = '1.0b3'
 # The full version, including alpha/beta/rc tags.
 release = '1.0b1'
 
