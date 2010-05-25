c10c5a24717a5519140ade95b4821ba4ff8ed923
sluggoster <sluggoster@gmail.com>
2010-05-24 22:24:44 -0700

    ``get_many()`` helper.

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -3,11 +3,13 @@ import os
 import copy
 import tempfile
 
-from nose.tools import eq_, raises
+from nose.tools import eq_
+
+from util import raises
 
 from webhelpers.containers import DumbObject
 from webhelpers.containers import defaultdict as webhelpers_containers_defaultdict
-from webhelpers.containers import distribute
+from webhelpers.containers import distribute, get_many
 
 # Tests from Python 2.5 test_defaultdict_defaultdict.py, as this is just a 2.4 backport
 # anyway
@@ -149,3 +151,21 @@ def test_distribute_with_extra():
         eq_(distribute(food, 3, "V", ""), [['apple', 'daikon', 'gelato'], ['banana', 'egg', 'honey'], ['carrot', 'fish', 'EXTRA']])
         eq_(distribute(food, 2, "H", ""), [['apple', 'banana'], ['carrot', 'daikon'], ['egg', 'fish'], ['gelato', 'honey'], ['EXTRA', '']])
         eq_(distribute(food, 2, "V", ""), [['apple', 'fish'], ['banana', 'gelato'], ['carrot', 'honey'], ['daikon', 'EXTRA'], ['egg', '']])
+
+def test_get_many():
+    # Dict with keys and values of 0, 1, 2, 3, 4
+    params = dict((n,n) for n in range(5))
+
+    eq_(get_many(params), [])
+    eq_(get_many(params, required=[1, 2]), [1, 2])
+    eq_(get_many(params, optional=[1, 2]), [1, 2])
+    eq_(get_many(params, optional=[6, 7, 8]), [None, None, None])
+    eq_(get_many(params, one_of=[1, 2]), [1])
+    eq_(get_many(params, one_of=[6, 1]), [1])
+
+    eq_(get_many(params, optional=[6, 1]), [None, 1])
+    eq_(get_many(params, required=[1, 2], optional=[6, 1]), [1, 2, None, 1])
+    eq_(get_many(params, required=[1, 2], optional=[6, 1], one_of=[7, 8, 3]), [1, 2, None, 1, 3])
+
+    raises(KeyError, get_many, params, required=[1, 6])
+    raises(KeyError, get_many, params, one_of=[7, 6])
